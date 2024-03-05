7235b68141fb00614fa0f1cc6b76793152b3c9fd
OmarLlinares <llinares.omar@gmail.com>
2024-03-05 13:42:39 +0100

    Commit 2
    6 Test

--- src/es/iesmz/EmpleadoBR.java
+++ src/es/iesmz/EmpleadoBR.java
@@ -2,47 +2,43 @@ package es.iesmz;
 
 public class EmpleadoBR {
 
-    private TipoEmpleado tipo;
-    private float salario;
-    private float ventaMes;
-    private int horasExtra;
+    private static TipoEmpleado tipo;
+    private static float salario;
+    private static float ventaMes;
+    private static int horasExtra;
 
     public EmpleadoBR(TipoEmpleado tipo, float ventaMes, int horasExtra) {
         this.tipo = tipo;
         this.ventaMes = ventaMes;
         this.horasExtra = horasExtra;
+
     }
 
-    public float calcularSalarioBruto(){
-        if (this == null || ventaMes < 0 || horasExtra < 0) {
-            if (tipo == TipoEmpleado.vendedor) {
-                salario = 1000;
-            }
-            if (tipo == TipoEmpleado.encarregat) {
-                salario = 1500;
-            }
-            if (ventaMes >= 1000) {
-                salario += 100;
-            } else if (ventaMes >= 1500) {
-                salario += 100;
-            }
-            salario += horasExtra * 20;
-            return salario;
+    public static float calcularSalarioBruto(TipoEmpleado tipo, float ventaMes, int horasExtra) {
+        salario = 0;
+        if (tipo == TipoEmpleado.encarregat) {
+            salario = 1500;
         }
-        return -1;
-    }
-
-    public float calcularSalarioNeto() {
-        int retencion = 0;
+        if (tipo == TipoEmpleado.venedor) {
+            salario = 1000;
+        }
+        if (ventaMes >= 1000) {
+            salario += 100;
+        }
+        if (ventaMes >= 1500) {
+            salario += 100;
+        }
         if (salario > 0) {
-            if (calcularSalarioBruto()>1000){
-                retencion = 16;
-            } else if (calcularSalarioNeto()>1500){
-                retencion = 18;
-            }
-            return calcularSalarioBruto()*retencion/100;
+            return salario += horasExtra * 20;
+        } else {
+            return -1;
         }
-        return -1;
     }
 
+    public static float calcularSalarioNeto(float salarioBruto) {
+        if (salarioBruto < 1000 && salarioBruto > 0) return salarioBruto;
+        if (salarioBruto >= 1000 && salarioBruto < 1500) return salarioBruto*0.84f;
+        if (salarioBruto >=1500) return salarioBruto*0.82f;
+        else return -1;
+    }
 }
--- src/es/iesmz/EmpleadoBRTest.java
+++ src/es/iesmz/EmpleadoBRTest.java
@@ -1,13 +1,39 @@
 package es.iesmz;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static es.iesmz.EmpleadoBR.calcularSalarioBruto;
+import static es.iesmz.EmpleadoBR.calcularSalarioNeto;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmpleadoBRTest {
 
-    @Test
-    public int getSaldo(){
+   @Test
+    public void testCalculaSalarioNeto2000to1640() {
+        assertEquals(1640, calcularSalarioNeto(2000) , 0.01);
+    }
 
+    @Test
+    public void testCalculaSalarioNeto1500to1230() {
+        assertEquals(1230, calcularSalarioNeto(1500) , 0.01);
     }
 
 
+    @Test
+    public void testCalculaSalarioNeto1499con99to1259con9916() {
+        assertEquals(1259.9916, calcularSalarioNeto(1499.99f) , 0.01);
+    }
 
+    @Test
+    public void testCalculaSalarioNeto1000to840() {
+        assertEquals(840, calcularSalarioNeto(1000) , 0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioNeto999con99to999con999() {
+        assertEquals(999.99f, calcularSalarioNeto(999.99f) , 0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioNeto500to500() {
+        assertEquals(500, calcularSalarioNeto(500) , 0.01);
+    }
 }
